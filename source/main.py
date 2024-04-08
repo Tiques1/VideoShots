89e1bdb9d9b5749a5b985613e70b7d54992bb272
Tiques1 <112898566+Tiques1@users.noreply.github.com>
2024-04-09 01:16:28 +0500

    Update main.py

--- source/main.py
+++ source/main.py
@@ -3,10 +3,15 @@ import recorder
 import keyboard
 import converter
 
-if __name__ == '__main__':
+def main():
 	activator = activator.Activator()
 	recorder = recorder.Recorder(directory='./examples/', convert=(converter.Format.GIF,))
 
 	activator.designate_area()
 	activator.start_record(recorder)
 	keyboard.add_hotkey('ctrl+alt+w', activator.stop_record)
+
+if __name__ == '__main__':
+	keyboard.add_hotkey('ctrl+alt+q', main)
+	while True:
+		time.sleep(0.1) # in order not to overload the system
